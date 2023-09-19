4659e74ace707a38b5196a4f0b67497ddb351a12
serbatinho <lserbato@gmail.com>
2023-09-19 17:08:14 -0300

    Style de about-section iniciado

--- src/components/Home/AboutSection.jsx
+++ src/components/Home/AboutSection.jsx
@@ -1,8 +1,44 @@
+import styles from "../../styles/layout/Home/about-section.module.scss"
+
+import { RiReactjsLine } from "react-icons/ri"
+
 export default function AboutSection() {
     return (
         <>
-            <section>
-                <h1>About me</h1>
+            <section className={`${styles['full-content-container']}`}>
+                <div className={`${styles['mid-content-container']}`}>
+                    <div className={`${styles['content-container']}`}>
+                        <div className={`${styles['about-item']}`}>
+                            <div className={`${styles['about-item-title']}`}>
+                                <h2>Python</h2>
+                                <a href="#"> < RiReactjsLine /> </a>
+
+                            </div>
+                            <p>De uma olhada em meus projetos python</p>
+                            <button>Conheça</button>
+                        </div>
+
+                        <div className={`${styles['about-item']}`}>
+                            <div className={`${styles['about-item-title']}`}>
+                                <h2>Figma</h2>
+                                <a href="#"> < RiReactjsLine /> </a>
+
+                            </div>
+                            <p>Desenvolvimento React e Node, treinamenti FIAP, Alura e Impacta</p>
+                            <button>Conheça</button>
+                        </div>
+
+                        <div className={`${styles['about-item']}`}>
+                            <div className={`${styles['about-item-title']}`}>
+                                <h2>Web</h2>
+                                <a href="#"> < RiReactjsLine /> </a>
+
+                            </div>
+                            <p>De uma olhada em meus projetos figma e front-end</p>
+                            <button>Conheça</button>
+                        </div>
+                    </div>
+                </div>
             </section>
         </>
     )
