19ce77e55472ca223f302d433c5f42ff4477be73
serbatinho <lserbato@gmail.com>
2023-09-28 05:31:32 -0300

    Sessão coworkers pronta

--- src/components/Home/Coworkers/Coworkers.jsx
+++ src/components/Home/Coworkers/Coworkers.jsx
@@ -1,32 +1,32 @@
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import { useRef, useState, useEffect } from "react";
 import { debounce } from "lodash";
 
 import { coworkersData } from "./data";
+
 import styles from "../../../styles/layout/Home/coworkers.module.scss";
+import { FaLinkedin, FaBriefcase } from "react-icons/fa";
 
 export default function Coworkers() {
     const [width, setWidth] = useState(0);
-
     const carousel = useRef();
     const innerCarousel = useRef();
+    const controls = useAnimation();
 
     const updateCarouselWidth = () => {
         const newWidth = innerCarousel.current.scrollWidth - innerCarousel.current.offsetWidth;
         setWidth(newWidth);
-        console.log(newWidth);
     };
 
-    const debouncedUpdateCarouselWidth = debounce(updateCarouselWidth, 500);
+    const returnCarousel = () => {
+        controls.start({ x: 0 }, { type: "spring", stiffness: 100, damping: 20 });
+    }
 
     useEffect(() => {
         updateCarouselWidth();
 
-        window.addEventListener("resize", debouncedUpdateCarouselWidth);
-
-        return () => {
-            window.removeEventListener("resize", debouncedUpdateCarouselWidth);
-        };
+        window.addEventListener("resize", debounce(updateCarouselWidth, 500))
+        window.addEventListener("resize", returnCarousel)
     }, []);
 
     return (
@@ -34,7 +34,7 @@ export default function Coworkers() {
             <section className={`${styles["full-content-container"]}`}>
                 <div className={`${styles["mid-content-container"]}`}>
                     <div className={`${styles["content-container"]}`}>
-                        <h2>Coworkers</h2>
+                        <h2>Colegas</h2>
                         <motion.section
                             ref={carousel}
                             whileTap={{ cursor: "grabbing" }}
@@ -45,19 +45,23 @@ export default function Coworkers() {
                                 drag="x"
                                 dragConstraints={{ right: 0, left: -width }}
                                 className={`${styles["inner-carousel"]}`}
+                                animate={controls}
                             >
                                 {coworkersData.map((item) => (
-                                    <motion.article className={`${styles["card-full"]}`} key={item.id}>
+                                    <motion.article
+                                        className={`${styles["card-full"]}`}
+                                        key={item.id}
+                                    >
                                         <h3 className={`${styles["card-title"]}`}>{item.name}</h3>
                                         <figure className={`${styles["card-full-img"]}`}>
                                             <div>
-                                                <img src={item.image} alt={item.name} />
+                                                <img src={item.linkImg} alt={item.name} />
                                             </div>
                                             <figcaption>{item.area}</figcaption>
                                         </figure>
                                         <nav className={`${styles["card-nav"]}`}>
-                                            <a href={item.linkedin}>inkedin</a>
-                                            <a href={item.portfolio}>Portfolio</a>
+                                            <a href={item.linkedin} target="_blank" rel="noreferrer"><FaLinkedin />LinkedIn</a>
+                                            <a href={item.portfolio} target="_blank" rel="noreferrer"><FaBriefcase />Portfolio</a>
                                         </nav>
                                         <p className={`${styles["card-desc"]}`}>{item.description}</p>
                                     </motion.article>
